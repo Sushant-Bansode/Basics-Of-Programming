5c5ff1aa71e2953cf2d421c6978797f1a76e7dbf
SUSHANT BANSODE <97442721+SUSHANTDBANSODE@users.noreply.github.com>
2022-07-11 20:48:43 +0530

    Updated

--- 03.Conditional & Loops/C7-Ternary_Operator.cpp
+++ 03.Conditional & Loops/C7-Ternary_Operator.cpp
@@ -1,47 +1,47 @@
-#include <stdio.h>
- 
+#include <iostream>
+using namespace std; 
 int main()
 {
     int n, n1, n2, n3, n4, max;
-    printf("ENTER UPTO 4 Numbers :\n ");
-    printf("HOW Many no. YOU ARE GOING TO COMPARE\n");
-    scanf("%d",&n);
+    cout << "ENTER UPTO 4 Numbers :\n ";
+    cout <<"HOW Many no. YOU ARE GOING TO COMPARE\n";
+    cin >> n;
     if( n==2 ){
-    printf( "enter 1st No.\n");
-    scanf("%d",&n1);
-     printf( "enter 2nd No.\n");
-    scanf("%d",&n2);
-    max = (n1 > n2) ? n1 : n2 ;
-    printf("YOUR MAXIMUM NO. IS %d",max);
+    cout << "enter 1st No.\n";
+    cin >> n1;
+     cout << "enter 2nd No.\n";
+    cin >> n2;
+    max = n1 > n2 ? n1 : n2 ;
+    cout << "YOUR MAXIMUM NO. IS " << max ;
 
     }
    else if ( n==3 ){
-    printf( "enter 1st No.\n");
-    scanf("%d",&n1);
-     printf( "enter 2nd No.\n" );
-    scanf("%d",&n2);
-     printf( "enter 3rd No.\n");
-    scanf("%d",&n3);
+    cout << "enter 1st No.\n";
+    cin >> n1;
+     cout << "enter 2nd No.\n";
+    cin >> n2;
+     cout << "enter 3rd No.\n";
+    cin >> n3;
     
     max = (n1 > n2 && n1 > n3 ) ?
                n1 : (n2 > n3) ?
                n2 : n3;
-printf("YOUR MAXIMUM NO. IS %d",max);
+cout << "YOUR MAXIMUM NO. IS " << max;
     }
     else if ( n==4 ){
-    printf( "enter 1st No.\n" );
-    scanf("%d",&n1);
-     printf( "enter 2nd No.\n" );
-    scanf("%d",&n2);
-     printf( "enter 3rd No.\n" );
-    scanf("%d",&n3);
-     printf( "enter 4th No.\n" );
-    scanf("%d",&n4);
+    cout << "enter 1st No.\n";
+    cin >> n1;
+     cout << "enter 2nd No.\n";
+    cin >> n2;
+     cout << "enter 3rd No.\n";
+    cin >> n3;
+     cout << "enter 4th No.\n";
+    cin >> n4;
     
     max = (n1 > n2 && n1 > n3 && n1 > n4) ?
                n1 : ((n2 > n3 && n2 > n4) ?
                n2 : (n3 > n4 ? n3 : n4));
-printf("YOUR MAXIMUM NO. IS %d",max);
+cout << "YOUR MAXIMUM NO. IS " << max;
     }
 
 
